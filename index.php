994eafd08a271f155229771554b453bcc53a4258
Santiagodespontin <santiagodespontin@gmail.com>
2018-08-12 21:07:30 -0300

    nav bar y footer incompletos

--- index.php
+++ index.php
@@ -3,11 +3,34 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Home page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
 </head>
 <body>
-    
+<header>
+
+</header>
+<nav>
+<ul>
+    <li><i class="fas fa-graduation-cap"></i></li>
+    <li><a href="#">Nombre</a></li>
+    <li id="registro"><a href="#">Registro</a></li>
+    <li><i class="fas fa-user"></i></li>
+</ul>
+</nav>
+<footer>
+<ul>
+    <li><a href="#">Terminos y condiciones</a></li>
+    <li>FAQ</li>
+    <li>
+        <ul class="redes">
+            <li><i class="fab fa-facebook-square"></i></li>
+            <li><i class="fab fa-twitter-square"></i></li>
+        </ul>
+    </li>
+</ul>
+</footer>
 </body>
 </html>
