9c41405422239dbe83e99d0bfee9cdd5e76f1411
BenjaminBergerM <benjaminbergermsurette@gmail.com>
2018-08-13 14:25:31 -0300

    Google Font

--- faq.php
+++ faq.php
@@ -6,6 +6,7 @@
         <title>FAQ</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
         <link rel="stylesheet" type="text/css" media="screen" href="css/main.css" />
 </head>
 <body>
@@ -18,7 +19,7 @@
             </div>
             <div>
                 <li id="registro"><a href="#">Registro</a></li>
-                <li class="user" ><i class="fas fa-user"></i></li>
+                <li class="user"><i class="fas fa-user"></i></li>
             </div>
         </ul>
     </nav>
