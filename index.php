7df705a028065782ee22654081ca5a7f8b6cd266
Santiagodespontin <santiagodespontin@gmail.com>
2018-08-15 16:47:30 -0300

    Arreglo de footer y categorias

--- index.php
+++ index.php
@@ -14,12 +14,12 @@
         <nav>
             <ul>
                 <div>
-                    <li><i class="fas fa-graduation-cap"></i></li>
+                    <li class="header-i" ><i class="fas fa-graduation-cap"></i></li>
                     <li><a class="nav-a" href="#">Nombre</a></li>
                 </div>
                 <div>
                     <li id="registro"><a class="nav-a" href="#">Registro</a></li>
-                    <li class="user" ><i class="fas fa-user"></i></li>
+                    <li class="header-i" ><i class="fas fa-user-circle"></i></li>
                 </div>
             </ul>
         </nav>
@@ -28,24 +28,39 @@
     <main>
         <h1>Encontra profesores en tres simples pasos</h1>
         <div>
+            <div class="main-i"><i class="fas fa-search"></i></div>
             <h2>Busca</h2>
         </div>
         <div>
+            <div class="main-i"><i class="fas fa-chalkboard-teacher"></i></div>
             <h2>Elegi</h2>
         </div>
         <div>
+            <div class="main-i"><i class="fab fa-leanpub"></i></div>
             <h2>Aprende</h2>
         </div>
+        <div class="div-cat">
+                <h3>Busca por categoria</h3>
+            <a class="categoria" href="#">Matematicas</a>
+            <a class="categoria" href="#">Historia</a>
+            <a class="categoria" href="#">Geografia</a>
+            <a class="categoria" href="#">Economia</a>
+            <a class="categoria" href="#">Teoria Contable</a>
+            <a class="categoria" href="#">Dibujo</a>
+            <a class="categoria" href="#">Ingles</a>
+            <a class="categoria" href="#">Frances</a>
+            <a class="categoria" href="#">Canto</a>
+            <a class="categoria" href="#">Baile</a>
+        </div>
     </main>
-
     <footer>
         <ul>
             <li><a href="#">Terminos y condiciones</a></li>
             <li>FAQ</li>
             <li>
                 <ul class="redes">
-                    <li><i class="fab fa-facebook-square"></i></li>
-                    <li><i class="fab fa-twitter-square"></i></li>
+                    <li><a href="https://www.facebook.com/"><i class="fab fa-facebook-square"></i></a></li>
+                    <li><a href="https://www.twitter.com/"><i class="fab fa-twitter-square"></i></a></li>
                 </ul>
             </li>
         </ul>
