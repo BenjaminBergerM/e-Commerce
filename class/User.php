facdeddc4a3cb70c305f429a774d9fdf094bbc64
Santiago <santiagodespontin@gmail.com>
2018-10-12 10:04:25 -0300

    DEtalles y prolijidad

--- class/User.php
+++ class/User.php
@@ -19,99 +19,57 @@ class User {
             $this->setFotoPerfil($fotoPerfil);
     }
 
-    /**
-     * Get the value of name
-     */ 
     public function getName()
     {
         return $this->name;
     }
 
-    /**
-     * Set the value of name
-     *
-     * @return  self
-     */ 
     public function setName($name)
     {
         $this->name = $name;
 
         return $this;
     }
-
-    /**
-     * Get the value of lastName
-     */ 
     public function getLastName()
     {
         return $this->lastName;
     }
 
-    /**
-     * Set the value of lastName
-     *
-     * @return  self
-     */ 
     public function setLastName($lastName)
     {
         $this->lastName = $lastName;
 
         return $this;
     }
-
-    /**
-     * Get the value of username
-     */ 
     public function getUsername()
     {
         return $this->username;
     }
 
-    /**
-     * Set the value of username
-     *
-     * @return  self
-     */ 
+
     public function setUsername($username)
     {
         $this->username = $username;
 
         return $this;
     }
-
-    /**
-     * Get the value of email
-     */ 
     public function getEmail()
     {
         return $this->email;
     }
 
-    /**
-     * Set the value of email
-     *
-     * @return  self
-     */ 
+
     public function setEmail($email)
     {
         $this->email = $email;
 
         return $this;
     }
-
-    /**
-     * Get the value of date
-     */ 
     public function getDate()
     {
         return $this->date;
     }
 
-    /**
-     * Set the value of date
-     *
-     * @return  self
-     */ 
     public function setDate($date)
     {
         $this->date = $date;
@@ -119,59 +77,35 @@ class User {
         return $this;
     }
 
-    /**
-     * Get the value of password
-     */ 
+    
     public function getPassword()
     {
         return $this->password;
     }
 
-    /**
-     * Set the value of password
-     *
-     * @return  self
-     */ 
     public function setPassword($password)
     {
         $this->password = $password;
 
         return $this;
     }
-
-    /**
-     * Get the value of comfirmPassword
-     */ 
     public function getComfirmPassword()
     {
         return $this->comfirmPassword;
     }
 
-    /**
-     * Set the value of comfirmPassword
-     *
-     * @return  self
-     */ 
     public function setComfirmPassword($comfirmPassword)
     {
         $this->comfirmPassword = $comfirmPassword;
 
         return $this;
     }
-
-    /**
-     * Get the value of fotoPerfil
-     */ 
     public function getFotoPerfil()
     {
         return $this->fotoPerfil;
     }
 
-    /**
-     * Set the value of fotoPerfil
-     *
-     * @return  self
-     */ 
+   
     public function setFotoPerfil($fotoPerfil)
     {
         $this->fotoPerfil = $fotoPerfil;
