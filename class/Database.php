9e9ac6a0b42828784125f17ae0f09bc3f538cfc1
Santiago <santiagodespontin@gmail.com>
2018-11-07 15:08:57 -0300

    SQL primeros pasos

--- class/Database.php
+++ class/Database.php
@@ -3,6 +3,8 @@
 abstract class Database {
     abstract public function bringUser($email);
     abstract public function saveUser(User $user);
+
+
 }
 
 ?>
--- class/MySQL.php
+++ class/MySQL.php
@@ -2,6 +2,27 @@
 
 class MySQL extends Database {
 
+    public function __construct() {}
+    protected function connect(){
+        $this->servername = "localhost";
+        $this->username = "root";
+        $this->password = "root";
+        $this->dbname = "movies_db";
+        
+        $db = new PDO($this->servername, $this->username,$this->password,$this->dbname);
+    
+}
+
+
+public function bringUser($email) {
+
+    }
+
+    public function saveUser(User $user) {
+
+    }
+
+
 }
 
 
